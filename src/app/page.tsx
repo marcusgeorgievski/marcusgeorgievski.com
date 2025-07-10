a955bbb39e4a4a3d252f13d677b5cdf5fead6504
marcusgeorgievski <marcusgeorgievski@outlook.com>
2025-07-10 17:57:56 -0400

    about text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ import { Socials } from "@/components/core/social";
 import ExperienceSection from "@/components/experience/experience-section";
 import MessageSection from "@/components/messages/message-section";
 import Link from "next/link";
+import { FaGraduationCap } from "react-icons/fa6";
 import { FiExternalLink } from "react-icons/fi";
+import { TbBriefcase2Filled } from "react-icons/tb";
 
 export default async function HomePage({
   searchParams,
@@ -61,14 +63,20 @@ function Header() {
 function About() {
   return (
     <div className="flex flex-col gap-4 text-slate-300">
-      <p>
-        Software Developer @ Dayforce, Computer Science @ University of Guelph.
-        Interested in distributed systems, cloud computing, and developer
-        experience.
-      </p>
+      <ul className="list-disc pl-0">
+        <li className="flex items-center gap-3">
+          <TbBriefcase2Filled /> Software Developer @ Dayforce
+        </li>
+        <li className="flex items-center gap-3">
+          <FaGraduationCap /> Computer Science @ University of Guelph
+        </li>
+      </ul>
 
       <p>
-        I also keep a log and personal reference notes on what I'm reading{" "}
+        Interested in distributed systems and the challenges of building
+        resilient, fault-tolerant architectures that maintain performance at
+        scale. I also keep a log and personal reference notes on what I'm
+        reading{" "}
         <Link
           href={"https://log.marcusgeorgievski.com/notes"}
           target="_blank"
